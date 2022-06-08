aeba0a9b6463297a93539978b650c1ba905e079f
Kim <92202385+Aguamalte@users.noreply.github.com>
2022-06-08 11:58:44 +0200

    Update account.js

--- account.js
+++ account.js
@@ -1,6 +1,6 @@
 
 // BANK ACCOUNT
-const atm=  parseFloat(prompt("Please select one of the following choices 1.See balance  2.Make a deposit  3.Make a withdrawal  4.Get account name  5.Exit"));
+
 
 const account = {
   accountName: "John Doe",
@@ -8,59 +8,75 @@ const account = {
   getBalance: function()
   {
     alert (`Your current balance is ${this.balance}`);
+    prompt(ATM());
   },
   deposit: function()
   {
     newDeposit = prompt("Type in the amount you would like to deposit");
-    if(newDeposit<=0,"",isNaN){
+    if(newDeposit<=0,"",isNaN(newDeposit)){
       account.accountError();
+      prompt(ATM());
     }
     else{
+      newDeposit+this.balance;
       alert(`You have deposited ${newDeposit}`);
+      prompt(ATM());
     }
   },
   withdrawal: function()
   {
     newWithdrawal = prompt("Type in the amount you would like to withdraw");
-    if(newWithdrawal<=0,"",/,^,\,d,+,$,/){
+    if(newWithdrawal<=0,"",isNaN(newWithdrawal)){
       account.accountError();
+      prompt(ATM());
     }
     else{
+      this.balance-newWithdrawal;
       alert(`You have withdrawn ${newWithdrawal}`)
+      prompt(ATM());
     }
   },
   getAccountName: function(){
     alert(`The accounts name is ${this.accountName}`);
+    prompt(ATM());
   },
   accountError: function(){
-    alert ("You cannot use negative numbers")
+    alert ("You have to input a valid number")
+    prompt(ATM());
     
   },
   exitAccount: function(){
     alert(`Shutting down`);
   }
-}
+}    
+     function ATM() {
+      const atm=  parseFloat(prompt("Please select one of the following choices 1.See balance  2.Make a deposit  3.Make a withdrawal  4.Get account name  5.Exit"));
+      // objekt atm med parseFloat och propmt här
+      if(atm ===1){
+        alert(`${account.getBalance()}`);
+       }
+       else if(atm === 2){
+        alert(`${account.deposit()}`); 
+       }
+       else if(atm === 3){
+        alert(`${account.withdrawal()}`);
+       }
+       else if(atm === 4){
+        alert(`${account.getAccountName()}`); 
+       }
+       else if(atm===5){
+          alert(`${account.exitAccount()}`);
+       }
+       else if(atm>5){
+        alert(`You need to enter a valid option`);
+      }
+       else if(atm<=0,"",isNaN){
+         account.accountError();
+         
+       }
+      // här lägger du if satsen rad 45 - 59
 
-     if(atm ===1){
-      alert(`${account.getBalance()}`);
-     }
-     else if(atm === 2){
-      alert(`${account.deposit()}`); 
-     }
-     else if(atm === 3){
-      alert(`${account.withdrawal()}`);
-     }
-     else if(atm === 4){
-      alert(`${account.getAccountName()}`); 
-     }
-     else if(atm===5){
-        alert(`${account.exitAccount()}`);
-     }
-     else if(atm>5){
-      alert(`You need to enter a valid option`);
+      // glöm inte kalla på din funktion för att starta.
     }
-     else if(atm<0){
-       account.accountError();
-     }
-
+prompt(ATM());
 
