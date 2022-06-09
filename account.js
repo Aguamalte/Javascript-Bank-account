6cef2ef39f6ef5f37c09cf4fd8bf36c5d11946bb
Kim <92202385+Aguamalte@users.noreply.github.com>
2022-06-09 15:04:33 +0200

    Update account.js

--- account.js
+++ account.js
@@ -6,82 +6,84 @@ const account = {
   balance: 100,
   getBalance: function()
   {
-    
     alert (`Your current balance is ${this.balance}`);
-    parseFloat(prompt(ATM()));
+    ATM();
   },
   deposit: function()
   {
     newDeposit = parseFloat(prompt("Type in the amount you would like to deposit"));
     if(newDeposit<=0,"",isNaN(newDeposit)){
       account.accountError();
-      
+      account.deposit();
     }
     else{
       this.balance+=newDeposit;
       alert(`You have deposited $${newDeposit}, you now have a balance of ${this.balance}`);
-      parseFloat(prompt(ATM()));
+      ATM();
     }
   },
   withdrawal: function()
   {
-    newWithdrawal = prompt("Type in the amount you would like to withdraw");
+    newWithdrawal = parseFloat(prompt("Type in the amount you would like to withdraw"));
     if(newWithdrawal<=0,"",isNaN(newWithdrawal)){
       account.accountError();
-      
+      account.withdrawal();
     }
-    else if (newWithdrawal <= this.balance) {
-				this.balance -= newWithdrawal;
-				alert("Transaction successful!");
-				alert(`Your remaining balance is $${this.balance}`);
-				ATM();
+    else if (newWithdrawal>this.balance) {
+				alert(`You cannot withdraw a larger amount than your current balance`)
+        account.withdrawal();
 			}
-      alert(`You have withdrawn $${newWithdrawal},you now have a balance of ${this.balance}`);
-      
-    }
-  }
+      else{
+        this.balance -= newWithdrawal;
+				alert(`Your remaining balance is $${this.balance}`);
+        ATM();
+      }
+    },
+  
   getAccountName: function(){
     alert(`The accounts name is ${this.accountName}`);
-    
+    ATM();   
   },
   accountError: function(){
     alert ("You have to input a valid number")
-    
-    
-  },
-  exitAccount: function(){
-    alert(`Shutting down`);
   }
+  
+}
+
+function exitAccount (){
+  return (`Shutting down`);
 }
    
      function ATM() {
-      const atm=  parseFloat(prompt("Please select one of the following choices 1.See balance  2.Make a deposit  3.Make a withdrawal  4.Get account name  5.Exit"));
+      
+      const opt =  parseFloat(prompt("Please select one of the following choices 1.See balance  2.Make a deposit  3.Make a withdrawal  4.Get account name  5.Exit"));
       // objekt atm med parseFloat och propmt här
-      if(atm ===1){
+      if(opt ===1){
         alert(`${account.getBalance()}`);
        }
-       else if(atm === 2){
+       else if(opt === 2){
         alert(`${account.deposit()}`); 
        }
-       else if(atm === 3){
+       else if(opt === 3){
         alert(`${account.withdrawal()}`);
        }
-       else if(atm === 4){
+       else if(opt === 4){
         alert(`${account.getAccountName()}`); 
        }
-       else if(atm===5){
-          alert(`${account.exitAccount()}`);
+       else if(opt===5){
+          alert(`${exitAccount()}`);
        }
-       else if(atm>5){
+       /* else if(atm>5){
         alert(`You need to enter a valid option`);
-        prompt(ATM());
-      }
-       else if(atm<=0,"",isNaN){
+        account.accountError(); */
+      
+       else if(opt<=0,"",isNaN,opt>5){
          account.accountError(); 
+         ATM();
        }
       // här lägger du if satsen rad 45 - 59
 
       // glöm inte kalla på din funktion för att starta.
     }
-parseFloat(prompt(ATM()));
+ATM();
 
